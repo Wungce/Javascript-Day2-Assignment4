feec9cb35b42628fcbfbd837724c973bc88487d2
wangsh <wugcelama19@gmail.com>
2021-06-18 16:59:04 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const Fruit_Inventory ={
+
+    apple:{
+        price:100,
+        quentity:200
+    },
+
+    mango:{
+        price:200,
+        quentity:300
+    },
+
+    orange:{
+        price:50,
+        quentity:555
+    },
+         
+    banana:{
+        price:25,
+        quentity:645
+    },
+
+    grapes:{
+        price:100,
+        quentity:799
+    }
+}
+
+const Apple_Quentity = 4;
+
+const Mango_Quentity = 4;
+
+const Orange_Quentity = 4;
+
+
+
+
+
+
+console.log(Fruit_Inventory["apple"] ["price"]*Apple_Quentity);
+
+console.log(Fruit_Inventory["mango"] ["price"]*Mango_Quentity);
+
+console.log(Fruit_Inventory["orange"] ["price"]*Orange_Quentity);
+
+
